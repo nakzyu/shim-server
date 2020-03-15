d8ee6ad5374f683d1eb9a3cc6129b36867497dc0
cha <nakzyu@gmail.com>
2020-03-15 14:51:43 +0900

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
