76ed908d963fe7cc24274157f7dd9cfa5e1d8736
cha <nakzyu@gmail.com>
2020-04-02 02:49:07 +0900

    before deploy

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-gup2v.gcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
-  .then(app.listen(5000))
+  .then(process.env.PORT || app.listen(5000))
   .catch(err => {
     console.log(err);
   });
